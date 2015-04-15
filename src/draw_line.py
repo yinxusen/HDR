f20424b12f545ad21644309a15ccbb74594d888e
Xusen Yin <yinxusen@gmail.com>
2015-04-15 17:22:30 +0800

    fix style and add aligned plot

--- src/draw_line.py
+++ src/draw_line.py
@@ -6,17 +6,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def splitData(filename1, filename2):
+def filteredData(filename1, filename2):
     file1 = pd.read_csv(filename1, sep = ",", header = 0)
     file2 = pd.read_csv(filename2, sep = ",", header = 0)
-    bond_head1 = file1.merge(file2, left_on="UNIT", right_on="visual_id")
-    res_bin8 = bond_head1[(bond_head1["BOND_HEAD"] == "TCB1") & (bond_head1["DIE"] == " [8PB]") & (bond_head1["interface_bin"] == 8)]
-    res_binx = bond_head1[(bond_head1["BOND_HEAD"] == "TCB1") & (bond_head1["DIE"] == " [8PB]") & (bond_head1["interface_bin"] != 8)]
-    #res_bin8_sm = res_bin8[res_bin8["substrate_vendor"] == "SM"]
-    #res_bin8_sh = res_bin8[res_bin8["substrate_vendor"] == "SH"]
-    #res_binx_sm = res_binx[res_binx["substrate_vendor"] == "SM"]
-    #res_binx_sh = res_binx[res_binx["substrate_vendor"] == "SH"]
-    return (res_bin8, res_binx)
+    bondHead1 = file1.merge(file2, left_on="UNIT", right_on="visual_id")
+    bondHead1 = bondHead1[pd.notnull(bondHead1["#Time"])]
+    data = bondHead1[(bondHead1["BOND_HEAD"] == "TCB1") & (bondHead1["DIE"] == " [8PB]") & \
+        ((bondHead1["interface_bin"] == 8) | (bondHead1["interface_bin"] == 1))]
+    return data
+
+def splitData(data):
+    resBin8 = data[data["interface_bin"] == 8]
+    resBin1 = data[data["interface_bin"] == 1]
+    return (resBin8, resBin1)
 
 def plotCompare(filename1, filename2):
     a, b = splitData(filename1, filename2)
@@ -31,31 +33,43 @@ def reIndexTime(oriTime, fname, state, dictOfOriStartPoints, dictOfNewStartPoint
     newStartPoint = dictOfNewStartPoints[state - 1]
     return oriTime - oriStartPoint + newStartPoint
 
-def main(mypath, content):
-    files = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
-    
-    # 1. find max intervals of each stage in a dir
-    fab = pd.DataFrame()
-    for f in files:
-        frame = pd.read_csv(join(mypath, f), sep="\t", header=0, skiprows=70)
-        frame['fname'] = f
-        fab = fab.append(frame, ignore_index = True)
-    
-    intervals = fab.groupby(["fname", "State"])["Force(g)"].count()
+def findMaxItervals(data, attr):
+    intervals = data.groupby(["FILE", "State"])[attr].count()
     max_intervals = intervals.max(level="State")
     cumsum_intervals = max_intervals.cumsum()
-    
-    # 2. align each stage with its corresponding largest interval
-    start_points = fab.groupby(["fname", "State"])["#Time"].agg(np.min)
-    fab["newTime"] = fab[["#Time", "fname", "State"]].apply(lambda row: \
+    return cumsum_intervals
+
+def alignIntervals(data, cumsumIntervals):
+    startPoints = data.groupby(["FILE", "State"])["#Time"].agg(np.min)
+    newTime = data[["#Time", "FILE", "State"]].apply(lambda row: \
         reIndexTime(row["#Time"], \
-        row["fname"], row["State"], start_points, cumsum_intervals), axis=1)
+        row["FILE"], row["State"], startPoints, cumsumIntervals), axis=1)
+    return newTime
+
+def readDirOfLogFiles(myPath):
+    files = [ f for f in listdir(myPath) if isfile(join(myPath,f)) ]
+    data = pd.DataFrame()
+
+    for f in files:
+        frame = pd.read_csv(join(myPath, f), sep="\t", header=0, skiprows=70)
+        frame['FILE'] = f
+        data = data.append(frame, ignore_index = True)
+
+    return data
+
+def main(attr):
+    filename1 = "/Users/panda/data/tmp/8513B027.csv"
+    filename2 = "/Users/panda/data/tmp/TCBd2.csv"
+    data = filteredData(filename1, filename2)
+    cumsumIntervals = findMaxItervals(data, attr)
+    newTime = alignIntervals(data, cumsumIntervals)
+    data["newTime"] = newTime
+    badBin, goodBin = splitData(data)
     
-    # 3. print each graph out
-    plt.plot(fab["newTime"], fab[content], '--', linewidth=2)
-    for v in cumsum_intervals:
+    plt.scatter(goodBin["newTime"], goodBin[attr])
+    plt.scatter(badBin["newTime"], badBin[attr])
+    for v in cumsumIntervals:
         plt.axvline(v)
     plt.show()
 
-#main("/Users/panda/data/tmp", sys.argv[1])
-plotCompare("/Users/panda/data/tmp/8513B027.csv", "/Users/panda/data/tmp/TCBd2.csv")
+main("Force(g)")
